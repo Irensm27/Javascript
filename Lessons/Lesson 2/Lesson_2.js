3a7fd6a4c24a8423e1ab39fe00a3a2bb4f8b60b9
irensm27 <semenets.irina27@gmail.com>
2025-11-16 20:16:45 +0200

    Lesson 2

---
+++ Lessons/Lesson 2/Lesson_2.js
@@ -0,0 +1,82 @@
+//-------------- МАСИВИ-----------------------
+// let arr;
+// arr = [123, 234, 345, -123, 0, true, 'asd'];
+// console.log(arr);
+// console.log(arr[2]);
+// console.log(arr.length);
+//
+// arr[0] = 'new value';
+// console.log(arr);
+//
+// arr[7] = 'okten';
+// console.log(arr);
+// arr[arr.length] =  123456;
+// console.log(arr);
+
+// let arr = [
+//     [11,22,33],
+//     [],
+//     []
+// ];
+// console.log(arr);
+// console.log(arr[0]);
+// let innerArray = arr[0];
+// console.log(innerArray[1]);
+//
+// console.log(arr[0][1]);
+//--------------------------- ОБ'ЄКТИ------------------------
+
+//
+// let user1 = {
+//     id: 1,
+//     name: "Ira",
+//     age: 27,
+//     skills: ['html' , 'javascript'],
+//     wife: {
+//         name: 'Olya',
+//         age: 25,
+//     }
+// }
+// console.log(user1);
+// console.log(user1.id);
+// console.log(user1['age']);
+// console.log(user1.skills);
+// console.log(user1.skills[0]);
+// console.log(user1.wife.name);
+
+// -----------------МАСИВИ З ОБ'ЄКТАМИ----------------------
+// let users = [
+//     {skills: ['html','mysql','mongo'], id:1, name:'vasya', age:31, status: true},
+//     {skills: ['html','js'],id:2, name:'petya', age:33, status: false},
+//     {skills: ['html','js'],id:3, name:'anna', age:28, status: true},
+//     {skills: ['html','mysql','mongo'],id:4, name:'olga', age:29, status: false}
+// ];
+// console.log(users);
+// let user0 = users[0];
+// console.log(user0.age);
+// console.log(users[0].skills[2]);
+// console.log(users[0]['skills']);
+//-------------------------- МОДИФІКАЦІЯ ОБ'ЄКТІВ------------------------
+// let user = {
+//     id: 1,
+//     name: "vasya",
+// };
+// user.age = 31;
+// user['status'] = false;
+// console.log(user);
+//
+// delete  user.id;
+// console.log(user);
+//
+// ----------------ПРИМІТИВИ ТА РЕФЕРЕНЦІАЛЬНІ ТИПИ-------------------------
+// let a = 100;
+// let b = a;
+// b = b + 10;
+//
+// let user = {name:'vasya'};
+// let user2 = user;
+// user2.age = 31;
+// console.log(user2);
+// console.log(user);
+// ----------------ЛОГІЧНІ РОЗГАЛУДЖЕННЯ IF-------------------------
+
