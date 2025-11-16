ab62c9b6dd705103f31bf7e317e2b1755dc220c0
irensm27 <semenets.irina27@gmail.com>
2025-11-16 18:03:46 +0200

    Homework 1

---
+++ Lessons/Lesson 1/Lesson 1.js
@@ -0,0 +1,98 @@
+// {
+//     let a = 'Hello';
+//
+//     var b = 'World';
+// }
+//     console.log(b);
+//     console.log(a);
+
+// const PI = 3.14;
+// console.log(PI);
+//
+// let a = 'Okey';
+// console.log(a);
+// a = 'No'
+// console.log(a);
+
+// let a = 'Hello okten';
+// // a = '5';
+// console.log(typeof a, a);
+// let b = 100500;
+// console.log(typeof b, b);
+//
+// let c = true;
+// console.log(c);
+//----------------STRING------------------------
+// let greeting = "Hello";
+// let intro = "my name is awesome!";
+// let msg = greeting + intro;
+// console.log(msg);
+// let msg2 = `${greeting}  ${intro}`;
+// console.log(msg2);
+// --------------------INTEGER/FLOAT-----------------------
+// let a = 100500;
+// console.log(a);
+// let b = 3.123565455;
+// console.log(b);
+//
+// console.log(10+20);
+// console.log(11 % 3);
+//
+// console.log(0.1+0.2);
+//
+// let big = 55655555555555555555555555555555555555555555n;
+// console.log(big);
+// let n1 = 100;
+// let n2 = 100;
+// console.log(n1+n2);
+// -------------------BOOLEAN-----------------------------
+// let a = true;
+// // console.log(typeof a);
+// // console.log(10 > 5);
+// // console.log(10 < 5);
+// // console.log(10 === 10);
+// // console.log(10 !== 10);
+//  console.log(5 === "5");
+//  console.log('asd' === 'asd');
+//  console.log('asd' > 'qwe');
+//  console.log(5 != '6');
+
+//-------------------------------------
+// console.log(typeof (100500 + ''));
+// console.log(100 + 200 + '');
+// console.log('' + 200 + 300);
+
+// console.log(+'100500');
+// console.log(parseInt('100500asd'));
+// console.log(parseInt('1000.123'));
+// console.log(parseFloat('1000.123'));
+
+// console.log(!!1000);
+// console.log(!!0);
+// console.log(!!'asd');
+// console.log(!!'');
+// console.log(!!'0');
+
+// console.log(true +'');
+// console.log(+true);
+
+// -------------------TYPEOF--------------------
+let n = 100;
+let s = 'asd';
+let b = true;
+
+console.log(typeof n);
+console.log(typeof s);
+console.log(typeof b);
+
+// let x = undefined;
+// console.log(typeof x);
+
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof NaN);
+
+console.log(!!0);
+console.log(!!null);
+console.log(!!undefined);
+console.log(!!'');
