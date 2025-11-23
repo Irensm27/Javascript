6d9d650f800b44662aa2121d40e7ab2fd71f3c7d
irensm27 <semenets.irina27@gmail.com>
2025-11-23 16:12:27 +0200

    Hw 2.3

---
+++ Homework/Homework 2/Hw_2.2.js
@@ -0,0 +1,4 @@
+let book1 = {title:'A', pageCount:287, genre:'fiction'};
+let book2 = {title:'B', pageCount:344, genre:'thriller'};
+let book3 = {title:'C', pageCount:199, genre:'fantasy'};
+
---
+++ Homework/Homework 2/Hw_2.3.js
@@ -0,0 +1,27 @@
+let book1 = {
+    title:'A',
+    pageCount:100,
+    genre:'fantasy',
+    authors: [
+        {name:'Maria', age:18},
+        {name:'Artem', age:27}
+    ]
+};
+let book2 = {
+    title:'B',
+    pageCount:111,
+    genre:'romance',
+    authors: [
+        {name:'Iryna', age:31},
+        {name:'Vlad', age:26}
+    ]
+};
+let book3 = {
+    title:'C',
+    pageCount:213,
+    genre:'horror',
+    authors: [
+        {name:'Kostya', age:38},
+        {name:'Natasha', age:45}
+    ]
+};
