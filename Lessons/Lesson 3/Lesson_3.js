36badb18a9225559159550939a80bc48a03939fa
irensm27 <semenets.irina27@gmail.com>
2025-11-29 17:55:41 +0200

    Lesson 3

---
+++ Lessons/Lesson 3/Lesson_3.js
@@ -0,0 +1,56 @@
+// ------------------------INCREMENTS-----------------------------
+// let x = 0;
+// x++;
+// x=x+1;
+// console.log(x);
+// x+=1;
+// console.log(x);
+//
+// x = x-1;
+// x-=1;
+// console.log(x);
+// x--;
+// console.log(x);
+
+// let a = 0;
+// let b = ++a;
+// console.log(b, a);
+
+// -----------------------FOR OF----------------------------------------
+// let users = [
+//     {name: 'Vasya', age:27, status: false},
+//     {name: 'Olya', age:27, status: false},
+//     {name: 'Ira', age:27, status: false},
+//     {name: 'Kate', age:27, status: false},
+//     {name: 'Vanya', age:27, status: false},
+//     {name: 'Kolya', age:27, status: false},
+//     {name: 'Vasya', age:27, status: false},
+//     {name: 'Kristina', age:27, status: false},
+//     {name: 'Vasya', age:27, status: false},
+//     {name: 'Vasya', age:27, status: false},
+//     {name: 'Vasya', age:27, status: false}
+// ];
+// debugger;
+// for (let user of users) {
+//     if (!user.status) {
+//         console.log(user)
+//     }
+// }
+// ----------------------------FOR------------------------------------------
+let users = [
+    {name: 'Vasya', age:27, status: false},
+    {name: 'Olya', age:32, status: true},
+    {name: 'Ira', age:14, status: false},
+    {name: 'Kate', age:28, status: true},
+    {name: 'Vanya', age:24, status: false},
+    {name: 'Kolya', age:36, status: false},
+    {name: 'Vasya', age:20, status: true},
+    {name: 'Kristina', age:25, status: false},
+    {name: 'Vasya', age:17, status: false},
+    {name: 'Vasya', age:45, status: false},
+    {name: 'Vasya', age:54, status: false}
+];
+for (let i = 0; i < users.length; i++) {
+    let user = users[i];
+    document.write(`<h3>${users[i].name}</h3>`)
+}
