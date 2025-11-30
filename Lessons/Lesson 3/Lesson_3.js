cc6aeeebfd9e262eca3d1be00fa56f7d7059b77b
irensm27 <semenets.irina27@gmail.com>
2025-11-30 15:07:23 +0200

    Lesson 3

--- Lessons/Lesson 3/Lesson_3.js
+++ Lessons/Lesson 3/Lesson_3.js
@@ -37,20 +37,71 @@
 //     }
 // }
 // ----------------------------FOR------------------------------------------
-let users = [
-    {name: 'Vasya', age:27, status: false},
-    {name: 'Olya', age:32, status: true},
-    {name: 'Ira', age:14, status: false},
-    {name: 'Kate', age:28, status: true},
-    {name: 'Vanya', age:24, status: false},
-    {name: 'Kolya', age:36, status: false},
-    {name: 'Vasya', age:20, status: true},
-    {name: 'Kristina', age:25, status: false},
-    {name: 'Vasya', age:17, status: false},
-    {name: 'Vasya', age:45, status: false},
-    {name: 'Vasya', age:54, status: false}
-];
-for (let i = 0; i < users.length; i++) {
-    let user = users[i];
-    document.write(`<h3>${users[i].name}</h3>`)
-}
+// let users = [
+//     {name: 'Vasya', age:27, status: false},
+//     {name: 'Olya', age:32, status: true},
+//     {name: 'Ira', age:14, status: false},
+//     {name: 'Kate', age:28, status: true},
+//     {name: 'Vanya', age:24, status: false},
+//     {name: 'Kolya', age:36, status: false},
+//     {name: 'Vasya', age:20, status: true},
+//     {name: 'Kristina', age:25, status: false},
+//     {name: 'Vasya', age:17, status: false},
+//     {name: 'Vasya', age:45, status: false},
+//     {name: 'Vasya', age:54, status: false}
+// ];
+// for (let i = 0; i < users.length; i++) {
+//     let user = users[i];
+//     document.write(`<h3>${users[i].name}</h3>`)
+// }
+// -----------------------------FOR IN--------------------------------------
+// let user = {
+//     id: 1,
+//     name: 'vasya',
+//     status: true,
+// }
+// for (let fieldName in user) {
+//     console.log(user[fieldName]);
+// }
+// let users = [
+//     {name: 'Vasya', age:27, status: false},
+//     {name: 'Olya', age:32, status: true},
+//     {name: 'Ira', age:14, status: false},
+//     {name: 'Kate', age:28, status: true},
+//     {name: 'Vanya', age:24, status: false},
+//     {name: 'Kolya', age:36, status: false},
+//     {name: 'Vasya', age:20, status: true},
+//     {name: 'Kristina', age:25, status: false},
+//     {name: 'Vasya', age:17, status: false},
+//     {name: 'Vasya', age:45, status: false},
+//     {name: 'Vasya', age:54, status: false}
+// ];
+// for (const user of users) {
+//     for (let fieldName in user){
+//         console.log(fieldName, user[fieldName]);
+//     }
+// }
+//------------------------------ WHILE------------------------------------
+// let users = [
+//     {name: 'Vasya', age:27, status: false},
+//     {name: 'Olya', age:32, status: true},
+//     {name: 'Ira', age:14, status: false},
+//     {name: 'Kate', age:28, status: true},
+//     {name: 'Vanya', age:24, status: false},
+//     {name: 'Kolya', age:36, status: false},
+//     {name: 'Vasya', age:20, status: true},
+//     {name: 'Kristina', age:25, status: false},
+//     {name: 'Vasya', age:17, status: false},
+//     {name: 'Vasya', age:45, status: false},
+//     {name: 'Vasya', age:54, status: false}
+// ];
+//
+// let i =0;
+// while(i<users.length){
+//     console.log(users[i]);
+//     i++;
+//
+// }
+do{
+    console.log('dfd')
+}while(false)
