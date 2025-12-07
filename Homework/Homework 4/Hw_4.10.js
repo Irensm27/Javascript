f0f80d0ab6d0db5e5437b913756b64c558ed3560
irensm27 <semenets.irina27@gmail.com>
2025-12-07 17:13:15 +0200

    Homework_4.10

---
+++ Homework/Homework 4/Hw_4.10.js
@@ -0,0 +1,11 @@
+function arrayMin(numbers){
+    let min = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+        const number = numbers[i];
+        if(number<=min ){
+            min = number;
+        }
+    }
+    return min;
+}
+console.log(arrayMin([1,2,3,4,5, 568, -58, 12]));
