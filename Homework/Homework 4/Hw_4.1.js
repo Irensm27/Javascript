981c267a53763f4c54c8de2cdb32c7a2f64c5dc6
irensm27 <semenets.irina27@gmail.com>
2025-12-07 13:54:18 +0200

    Homework_4.2

---
+++ Homework/Homework 4/Hw_4.1.js
@@ -0,0 +1,6 @@
+function square(aNam,bNam){
+    let res = aNam*bNam;
+    console.log(res);
+    return res;
+}
+square(5, 15);
---
+++ Homework/Homework 4/Hw_4.2.js
@@ -0,0 +1,6 @@
+function roundSquare(radius){
+    let result = Math.PI*radius*radius;
+    console.log(result);
+    return result;
+}
+roundSquare(5);
