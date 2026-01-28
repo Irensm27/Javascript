03204ca0d579e6fac1b5d5cebec0e385d56a562b
irensm27 <semenets.irina27@gmail.com>
2026-01-28 20:40:14 +0200

    Lesson 8

---
+++ Lessons/Lesson 8/Lesson_8.js
@@ -0,0 +1,60 @@
+// let name = 'Vasya';
+// let age = 20;
+// let user = {
+//     name,
+//     age,
+//     foo(){
+//         console.log(this)
+//     }
+// }
+// console.log(user);
+// user.foo()
+
+//-----------ДЕСТРУКТУРИЗАЦІЯ-------------
+//
+// let user = {
+//     name: "John",
+//     age: 25,
+//     foo(){
+//         console.log("foo");
+//     }
+// }
+//
+// let {name} = user;
+// console.log(name)
+
+
+// let name = 'Vasya';
+// let age = 20;
+// let user = {
+//     name,
+//     age,
+//     foo(){
+//         console.log(this)
+//     }
+// }
+// let {name: userName, age: userAge}= user;
+// console.log(userAge);
+
+// let numbers = [11, 22, 33];
+// let [a, b] = numbers;
+// console.log(a, b);
+
+// let users = [
+//     {name: 'Vasya', age:27, status: false},
+//     {name: 'Olya', age:32, status: true},
+//     {name: 'Ira', age:14, status: false},
+//     {name: 'Kate', age:28, status: true},
+//     {name: 'Vanya', age:24, status: false},
+//     {name: 'Kolya', age:36, status: false},
+//     {name: 'Vasya', age:20, status: true},
+//     {name: 'Kristina', age:25, status: false},
+//     {name: 'Vasya', age:17, status: false},
+// ];
+// let [{name, age, status}, user2] = users;
+// console.log(user2);
+
+//---------SPRED------------------
+
+
+
