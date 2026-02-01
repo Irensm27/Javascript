f4e13dd61b35ea838f6b8a6305bb3d5406422e38
irensm27 <semenets.irina27@gmail.com>
2026-02-01 16:35:21 +0200

    Lesson 8

--- Lessons/Lesson 8/Lesson_8.js
+++ Lessons/Lesson 8/Lesson_8.js
@@ -55,6 +55,66 @@
 // console.log(user2);
 
 //---------SPRED------------------
+// let user = {
+//     name: "John",
+//     age: 25,
+// };
+// let userCopy = {...user};
+// userCopy.age = 50;
+// console.log(userCopy);
+//
+// let nums = [11, 22, 33];
+// let nums2 = [...nums];
+
+//------------------КОПІЇ-------------------
+// let user = {
+//     name: "John",
+//     skills: ['html','javascript','css'],
+// }
+// // let userClone= {...user};
+// let s = JSON.stringify(user);
+// console.log(s);
+// let parse = JSON.parse(s);
+//  let assign = Object.assign({}, user);
+//  console.log(assign);
+
+//----------PHOTOTYPING--------------
+
+// let base = {
+//     id: 1,
+//     name: "Javascript",
+// };
+// let copy = Object.create(base);
+// console.log(base);
+// console.log(copy.id);
+// console.log(copy);
+// copy.id = 2;
+// console.log(copy);
+// console.log(copy.hasOwnProperty('id'));
+// let obj = {};
+// obj.__proto__ = base;
+// console.log(obj);
+
+//-------------DESCRIPTORS---------------
+let user ={
+    firstName: "John",
+    lastName: "Doe",
+};
+// console.log(user);
+// user.firstName = "Taras";
+// for (const userElement in user) {
+//     console.log(userElement);
+// }
+Object.defineProperty(user, 'id', {value:100500, writable: false, enumerable: false, configurable: false});
+console.log(user.id);
+for (const userKey in user) {
+    console.log(userKey);
+}
+
+
+
+
+
 
 
 
