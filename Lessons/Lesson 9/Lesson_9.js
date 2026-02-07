79b5f479ddf234a12b4323cf62ef161f42c474f8
irensm27 <semenets.irina27@gmail.com>
2026-02-07 21:21:05 +0200

    Homework 8

---
+++ Lessons/Lesson 9/Lesson_9.js
@@ -0,0 +1,13 @@
+// console.log(document);
+// console.log(document.head.children);
+// console.log(document.body);
+// let elementById = document.getElementById("list-1");
+// console.log(elementById);
+// let elementsByClassName = document.getElementsByClassName('menu');
+// console.log(elementsByClassName);
+// let elementsByTagName = document.getElementsByTagName('li');
+// console.log(elementsByTagName);
+// let elementsByTagName1 = elementById.getElementsByTagName('li');
+// console.log(elementsByTagName1);
+console.log(document.querySelector('ul'));
+console.log(document.querySelectorAll('li'));
