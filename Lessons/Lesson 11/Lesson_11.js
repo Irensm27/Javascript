5862e111e8464a03ebfe4a05de925469c019410e
irensm27 <semenets.irina27@gmail.com>
2026-03-15 17:00:10 +0200

    Lesson 11

--- Lessons/Lesson 11/Lesson_11.js
+++ Lessons/Lesson 11/Lesson_11.js
@@ -8,27 +8,27 @@
 //         }
 //     });
 
-// fetch('https://dummyjson.com/products?limit=100')
-//     .then(value => value.json())
-//     .then(res => {
-//         let {products} = res;
-//         console.log(products);
-//         document.write(`<div>`)
-//         for (const product of products) {
-//             document.write(`<div>`)
-//             document.write(`<p>${product.brand} - ${product.price}  </p>`)
-//             document.write(`<img src="${product.thumbnail}" alt = "">`)
-//             document.write(`</div>`)
-//         }
-//         document.write(`</div>`)
-//     });
-fetch('https://dummyjson.com/products/add', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-        title: 'BMW Pencil',
-        /* other product data */
-    })
-})
-    .then(res => res.json())
-    .then(console.log);
+fetch('https://dummyjson.com/products?limit=100')
+    .then(value => value.json())
+    .then(res => {
+        let {products} = res;
+        console.log(products);
+        document.write(`<div>`)
+        for (const product of products) {
+            document.write(`<div>`)
+            document.write(`<p>${product.brand} - ${product.price}  </p>`)
+            document.write(`<img src="${product.thumbnail}" alt = "">`)
+            document.write(`</div>`)
+        }
+        document.write(`</div>`)
+    });
+// fetch('https://dummyjson.com/products/add', {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify({
+//         title: 'BMW Pencil',
+//         /* other product data */
+//     })
+// })
+//     .then(res => res.json())
+//     .then(console.log);
